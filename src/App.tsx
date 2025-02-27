203535ff38f445a363ba07023ab5a2ef90227f0b
장원정 <jang_expedition@gmail.com>
2025-02-28 03:40:34 +0900

    fix: lint 오류 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import { ScheduleProvider } from "./ScheduleContext.tsx";
 import { ScheduleTables } from "./ScheduleTables.tsx";
-import ScheduleDndProvider from "./ScheduleDndProvider.tsx";
 
 function App() {
   return (
